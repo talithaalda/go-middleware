fd9a5317c6f5f091abf234b01a5b72d9c809c9a2
talithaalda <talithaalda@gmail.com>
2024-03-16 02:13:58 +0700

    done

--- internal/repository/user_test.go
+++ internal/repository/user_test.go
@@ -1,68 +1,27 @@
 package repository
 
 import (
-	"context"
-	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/mock"
+	"gorm.io/gorm"
 
-	"github.com/talithaalda/go-middleware/internal/infrastructure"
-	"github.com/talithaalda/go-middleware/internal/model"
+	"github.com/talithaalda/go-middleware/internal/infrastructure/mocks"
 )
 
-type mockGormPostgres struct {
-	mock.Mock
-}
+func TestGetConnection(t *testing.T) {
+	// Setup
+	mockDB := new(mocks.GormPostgres)
+	expectedDB := &gorm.DB{} // Expected return value
 
-func (m *mockGormPostgres) GetConnection() infrastructure.DBConnection {
-	args := m.Called()
-	return args.Get(0).(infrastructure.DBConnection)
-}
+	mockDB.On("GetConnection").Return(expectedDB)
 
-func TestCreateUser_Success(t *testing.T) {
-	mockDB := new(mockGormPostgres)
-	mockDB.On("GetConnection").Return(&infrastructure.DBConnectionMock{})
+	// Execution
+	db := mockDB.GetConnection()
 
-	repo := NewUserQuery(mockDB)
+	// Assertion
+	assert.Equal(t, expectedDB, db)
 
-	expectedUser := model.User{
-		ID:       1,
-		Username: "testuser",
-		Email:    "test@example.com",
-	}
-
-	mockDBConnection := &infrastructure.DBConnectionMock{}
-	mockDBConnection.On("WithContext", mock.Anything).Return(mockDBConnection)
-	mockDBConnection.On("Table", "users").Return(mockDBConnection)
-	mockDBConnection.On("Save", mock.Anything).Return(nil)
-
-	user, err := repo.CreateUser(context.Background(), expectedUser)
-
-	assert.NoError(t, err)
-	assert.Equal(t, expectedUser, user)
-}
-
-func TestCreateUser_Error(t *testing.T) {
-	mockDB := new(mockGormPostgres)
-	mockDB.On("GetConnection").Return(&infrastructure.DBConnectionMock{})
-
-	repo := NewUserQuery(mockDB)
-
-	expectedUser := model.User{
-		ID:       1,
-		Username: "testuser",
-		Email:    "test@example.com",
-	}
-
-	mockDBConnection := &infrastructure.DBConnectionMock{}
-	mockDBConnection.On("WithContext", mock.Anything).Return(mockDBConnection)
-	mockDBConnection.On("Table", "users").Return(mockDBConnection)
-	mockDBConnection.On("Save", mock.Anything).Return(errors.New("unable to save user"))
-
-	user, err := repo.CreateUser(context.Background(), expectedUser)
-
-	assert.Error(t, err)
-	assert.Equal(t, model.User{}, user)
+	// Verify
+	mockDB.AssertExpectations(t)
 }
